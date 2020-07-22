04b148f37c59c8c9d04e51d21b20bfbd437f032d
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-07-22 18:16:35 +0900

    Update repeated_string.py

--- Interview-Preparation-Kit/Warmup-Challenges/repeated_string.py
+++ Interview-Preparation-Kit/Warmup-Challenges/repeated_string.py
@@ -9,26 +9,9 @@ import sys
 # Complete the repeatedString function below.
 def repeatedString(s, n):
     return s.count("a") * (n // len(s)) + s[:n % len(s)].count("a")
-    # first trial
-    # text = ""
-    # count = 0
-    # for i in range(n):
-    #     text += s
-    # substr = text[:n]
-    # for character in range(len(substr)):
-    #     if substr[character] == 'a':
-    #         count += 1
-    # return count
-
-    # second trial
-    # text = ""
-    # count = 0
-    # text = s * n
-    # substr = text[:n]
-    # return substr.count('a')
 
 if __name__ == '__main__':
-    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
 
@@ -36,6 +19,6 @@ if __name__ == '__main__':
 
     result = repeatedString(s, n)
 
-    #fptr.write(str(result) + '\n')
-    print(str(result) + '\n')
-    #fptr.close()
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
