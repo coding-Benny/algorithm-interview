66defebd30075ddfab9911dca5ddd1420437eacd
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-07-14 17:55:18 +0900

    ⌛ Find the maximum hourglass sum

--- HackerRank/Interview-Preparation-Kit/Arrays/2d_array.py
+++ HackerRank/Interview-Preparation-Kit/Arrays/2d_array.py
@@ -1,14 +1,8 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
 
 # Complete the hourglassSum function below.
-def hourglassSum(arr):
+def hourglassSum1(arr):
     values = []
     for i in range(4):
         for j in range(4):
@@ -21,16 +15,27 @@ def hourglassSum(arr):
     return max(values)
 
 
+def hourglassSum2(arr):
+    sum_list = []
+    for i in range(1, len(arr) - 1):
+        for j in range(1, len(arr[0]) - 1):
+            total = 0
+            total += arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1]
+            total += arr[i][j]
+            total += arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1]
+            sum_list.append(total)
+    return max(sum_list)
+
+
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    result = hourglassSum1([
+        [-9, -9, -9, 1, 1, 1], [0, -9, 0, 4, 3, 2], [-9, -9, -9, 1, 2, 3],
+        [0, 0, 8, 6, 6, 0], [0, 0, 0, -2, 0, 0], [0, 0, 1, 2, 4, 0]
+    ])
+    print(result)
 
-    arr = []
-
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
-
-    result = hourglassSum(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    result = hourglassSum2([
+        [1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0],
+        [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]
+    ])
+    print(result)
