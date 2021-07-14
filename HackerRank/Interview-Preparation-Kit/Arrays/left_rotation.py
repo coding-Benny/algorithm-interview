2515340613d5d0a8f34e5ea4a499712ed313b97b
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-07-14 20:33:25 +0900

    🔁 Shift elements in an array to the left

--- HackerRank/Interview-Preparation-Kit/Arrays/left_rotation.py
+++ HackerRank/Interview-Preparation-Kit/Arrays/left_rotation.py
@@ -1,30 +1,10 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-import collections
 
 # Complete the rotLeft function below.
 def rotLeft(a, d):
     return a[d:] + a[:d]
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    nd = input().split()
-
-    n = int(nd[0])
-
-    d = int(nd[1])
-
-    a = list(map(int, input().rstrip().split()))
-
-    result = rotLeft(a, d)
-
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-    
-    fptr.close()
+    result = rotLeft([1, 2, 3, 4, 5], 4)
+    print(result)
