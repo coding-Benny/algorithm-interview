98680288c2594ffe8a0d14656d41b6d18b4aadab
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-08-04 12:12:03 +0900

    😲 Add a concise solution

--- Programmers/Level2/sort-file-names.py
+++ Programmers/Level2/sort-file-names.py
@@ -1,3 +1,6 @@
+import re
+
+
 def split_part(files):
     # 각 구간을 |로 분리
     for idx, file in enumerate(files):
@@ -34,15 +37,21 @@ def solution(files):
     return files
 
 
+def solution2(files):
+    a = sorted(files, key=lambda file: int(re.findall('\d{1,5}', file)[0]))
+    b = sorted(a, key=lambda file: re.split('\d{1,5}', file.lower())[0])
+    return b
+
+
 def main():
     # ["F-15", "foo9.txt", "foo010bar020.zip"]
     print(solution(["foo9.txt", "foo010bar020.zip", "F-15"]))
     # ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
     print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
     # ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
-    print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
+    print(solution2(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
     # ["img000012345", "img1.png","img2","IMG02"]
-    print(solution(["img000002345", "img1.png", "img2", "IMG02"]))
+    print(solution2(["img000002345", "img1.png", "img2", "IMG02"]))
 
 
 if __name__ == '__main__':
