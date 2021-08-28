a23a999b5af58d8f2c59b753b3dc32bad3521f38
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-08-28 15:50:51 +0900

    ♻️ Refactor and add test case to run

--- LeetCode/Graph/subsets.py
+++ LeetCode/Graph/subsets.py
@@ -1,14 +1,26 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
+# https://leetcode.com/problems/subsets/
+from typing import List
 
-        def dfs(index, path):
-            # 매번 결과 추가
-            result.append(path)
 
-            # 경로를 만들면서 DFS
-            for i in range(index, len(nums)):
-                dfs(i + 1, path + [nums[i]])
+def subsets(nums: List[int]) -> List[List[int]]:
+    result = []
 
-        dfs(0, [])
-        return result
+    def dfs(index, path):
+        # 매번 결과 추가
+        result.append(path)
+
+        # 경로를 만들면서 DFS
+        for i in range(index, len(nums)):
+            dfs(i + 1, path + [nums[i]])
+
+    dfs(0, [])
+    return result
+
+
+def main():
+    print(subsets([1, 2, 3]))  # [[], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
+    print(subsets([0]))  # [[], [0]]
+
+
+if __name__ == '__main__':
+    main()
