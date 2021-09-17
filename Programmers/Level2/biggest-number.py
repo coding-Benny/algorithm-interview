614678a700c396fd95955844cc2ecf600effd53b
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-09-17 11:05:58 +0900

    ➕ Add another solutions

--- Programmers/Level2/biggest-number.py
+++ Programmers/Level2/biggest-number.py
@@ -1,4 +1,5 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42746
+import functools
 
 
 def to_swap(n1, n2):
@@ -17,14 +18,36 @@ def solution(numbers):
     return str(''.join(map(str, numbers))) if numbers[0] != 0 else "0"
 
 
+# another solution
+def solution2(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    return str(int(''.join(numbers)))
+
+
+def comparator(a, b):
+    t1 = a + b
+    t2 = b + a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2))  # t1이 크다면 1, t2가 크다면 -1,같으면 0
+
+
+# another solution
+def solution3(numbers):
+    n = [str(x) for x in numbers]
+    n.sort(key=functools.cmp_to_key(comparator), reverse=True)
+    return str(int(''.join(n)))
+
+
 def main():
     print(solution([6, 10, 2]))  # "6210"
     print(solution([3, 30, 34, 5, 9]))  # "9534330"
     print(solution([0, 0]))  # "0"
-    print(solution([33, 30, 3]))  # "33330"
-    print(solution([6, 646]))  # "6646"
-    print(solution([383, 38]))  # "38383"
-    print(solution([838, 83]))  # "83883"
+    print(solution2([33, 30, 3]))  # "33330"
+    print(solution2([6, 646]))  # "6646"
+    print(solution2([383, 38]))  # "38383"
+    print(solution3([838, 83]))  # "83883"
+    print(solution3([23, 232, 21, 212]))  # "2323221221"
+    print(solution3([9, 997, 99, 878, 87]))  # "99999787887"
 
 
 if __name__ == '__main__':
